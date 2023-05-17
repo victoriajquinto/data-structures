9d8a10cef1c3aa8390464b79ba74baa54059664f
victoriajquinto <victoriajquinto@gmail.com>
2023-05-17 16:09:57 -0700

    pass all tests for functional shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,51 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  let instance = {
+    storage: {},
+  };
+  _.extend(instance, stackMethods);
+  // console.log('instance: ', instance);
+  return instance;
 };
 
 var stackMethods = {};
+
+
+///where to put storage ....
+
+stackMethods.push = function (value) {
+  console.log('value to push', value);
+  let storage = this.storage;
+  //add properties to end of object
+  let length = Object.keys(storage).length;
+  console.log(length);
+  storage[length] = value;
+  console.log('storage', storage);
+  console.log('end push----');
+
+};
+
+stackMethods.pop = function () {
+  var lastValue;
+
+  let storage = this.storage;
+  let length = Object.keys(storage).length;
+  //remove properties at end of object
+  let lastKey = length - 1;
+  lastValue = storage[lastKey];
+  delete storage[lastKey];
+
+  return lastValue;
+
+};
+
+stackMethods.size = function () {
+  let storage = this.storage;
+  let length = Object.keys(storage).length;
+  return length;
+
+};
 
 
